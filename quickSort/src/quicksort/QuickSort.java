9b770de9f1b2ee3783679a378c4a4069f3b00d67
Lucas <lcsales98@gmail.com>
2018-06-19 10:48:35 -0300

    QuickSort Finalizado
    QuickSort completo, projeto encerrado.

--- quickSort/src/quicksort/QuickSort.java
+++ quickSort/src/quicksort/QuickSort.java
@@ -1,4 +1,4 @@
-/*
+
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,36 +11,65 @@ import java.util.Random;
  *
  * @author Aluno
  */
-public class QuickSort {
+public class Quicksort {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        // Determia as letras que poderão estar presente nas chaves
         int vetor[] = new int[20];
         Random gerador = new Random();
-        //imprime sequência de 10 números inteiros aleatórios
+        String letras = "ABCDEFGHIJKLMNOPQRSTUVYWXZ";
+        String letra = new String();
+        char caractere[] = new char [0];
+        char vetorchar[] = new char[20];
+        int index = -1;
+        
+        //gerando lista do vetor de caracteres não ordenado
+        for( int i = 0; i < 20 ; i++ ) {
+           index = gerador.nextInt( letras.length() );
+           letra = letras.substring( index, index + 1 );
+           caractere = letra.toCharArray();
+           vetorchar[i] = caractere[0];
+        }
+        System.out.println("Vetor de caracteres não ordenado:");
+        System.out.println( vetorchar );
+        System.out.println("");
+        
+        //ordena e imprime lista ordenada
+        quickSortChar(vetorchar, 0, vetorchar.length - 1);
+        System.out.println("Vetor de caracteres ordenado:");
+        System.out.println( vetorchar );
+        System.out.println("");
+        
+        //gerando lista do vetor numerico não ordenado
         for (int i = 0; i < 20; i++) {
             vetor[i] = gerador.nextInt(20);
         }
-        //lista o vetor não ordenado
+        
+        //imprime sequencia de 20 numeros inteiros aleatorios
+        System.out.println("Vetor numérico não ordenado:");
         for (int i = 0; i < vetor.length; i++) {
             System.out.print(" " + vetor[i]);
         }
-        quickSort(vetor, 0, vetor.length - 1);
+        quickSortNum(vetor, 0, vetor.length - 1);
         //lista o vetor Ordenado
-        System.out.println("");
+        System.out.println("\n\nVetor numérico ordenado:");
         for (int i = 0; i < vetor.length; i++) {
             System.out.print(" " + vetor[i]);
         }  
+        System.out.println("");
     }
-    public static void troca(int X[], int i, int j){
-        int aux;
+    
+    public static void trocaChar(char X[], int i, int j){
+        char aux;
         aux = X[i];
         X[i]= X[j];
         X[j]= aux;     
     }
-    public static int particao(int X[], int p, int r){
+    public static int particaoChar(char X[], int p, int r){
         int pivo, i, j;
         pivo = X[(p+r)/2];
         i = p - 1;
@@ -53,18 +82,54 @@ public class QuickSort {
                 i++;
             }while(X[i]<pivo);
             if (i<j){
-                troca(X,i,j);              
+                trocaChar(X,i,j);              
             }
         }
      
         return j;   
     }
-    public static void quickSort(int X[], int p, int r){
+    public static void quickSortChar(char X[], int p, int r){
         int q;
         if (p<r){
-            q = particao(X,p,r);
-            quickSort(X,p,q);
-            quickSort(X,q+1,r);
+            q = particaoChar(X,p,r);
+            quickSortChar(X,p,q);
+            quickSortChar(X,q+1,r);
+            
+        }
+       
+    }    
+    
+    public static void trocaNum(int X[], int i, int j){
+        int aux;
+        aux = X[i];
+        X[i]= X[j];
+        X[j]= aux;     
+    }
+    public static int particaoNum(int X[], int p, int r){
+        int pivo, i, j;
+        pivo = X[(p+r)/2];
+        i = p - 1;
+        j = r + 1;
+        while(i<j){
+            do{
+                j--;
+            }while(X[j]>pivo);
+            do{
+                i++;
+            }while(X[i]<pivo);
+            if (i<j){
+                trocaNum(X,i,j);              
+            }
+        }
+     
+        return j;   
+    }
+    public static void quickSortNum(int X[], int p, int r){
+        int q;
+        if (p<r){
+            q = particaoNum(X,p,r);
+            quickSortNum(X,p,q);
+            quickSortNum(X,q+1,r);
             
         }
         
